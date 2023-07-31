ce1ce73bd4008bbc6e75682c268a00ce6f536f71
Abhishek Mahato <101643561+abhishek-mahato0@users.noreply.github.com>
2023-07-31 19:15:16 +0545

    final commit

--- frontend/src/redux/constants/loginConstant.js
+++
@@ -1,7 +0,0 @@
-export const LOGIN_SUCCESS ="LOGIN_SUCCESS"
-export const LOGIN_FAIL ="LOGIN_FAIL"
-
-export const REGISTER_SUCCESS ="REGISTER_SUCCESS"
-export const REGISTER_FAIL ="REGISTER_FAIL"
-
-export const LOG_OUT="LOG_OUT"
