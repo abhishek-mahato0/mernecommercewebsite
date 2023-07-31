e6129d1da3ad7c6a7f1790af9cb6646ef8b8f194
Abhishek Mahato <101643561+abhishek-mahato0@users.noreply.github.com>
2023-07-31 19:04:13 +0545

    added url

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -89,7 +89,7 @@ userRoute.post('/login', async (req, res) => {
         });
         const { password, ...others } = user._doc;
         res
-          .cookie('token', token, {
+          .cookie('token', JSON.stringify(token), {
             // Options for the cookie
             maxAge: 24 * 60 * 60 * 1000,
             httpOnly: true,
